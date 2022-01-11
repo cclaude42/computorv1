5b13962e86c0460b4c6466e8f843102b99f74dd9
Cesar CLAUDE <cclaude@e2r5p8.clusters.42paris.fr>
2022-01-11 20:49:47 +0100

    fixed 1*X^0 disappearing, delta=0 solution, and added step-by-step solving in output

--- srcs/solve.cpp
+++ srcs/solve.cpp
@@ -2,7 +2,7 @@
 
 void reduced_form (POLY poly)
 {
-    std::string rform = "Reduced form:";
+    std::string rform = "Reduced form :";
 
     for (auto it = poly.begin() ; it != poly.end() ; it++) {
         std::stringstream ss;
@@ -15,7 +15,7 @@ void reduced_form (POLY poly)
             rform += " ";
             rform += (it->second >= 0) ? "+" : "-";
         }
-        if (ss.str() != "1") {
+        if (ss.str() != "1" || it->second == 0) {
             rform += " ";
             rform += ss.str();
         }
@@ -33,12 +33,12 @@ void reduced_form (POLY poly)
 
     rform += " = 0";
 
-    std::cout << rform << std::endl;
+    std::cout << rform << std::endl << std::endl;
 }
 
 void polynomial_degree (POLY poly)
 {
-    std::string pdegree = "Polynomial degree: ";
+    std::string pdegree = "Polynomial degree : ";
 
     pdegree += std::to_string(poly.rbegin()->first);
 
@@ -47,8 +47,8 @@ void polynomial_degree (POLY poly)
 
 void solve_zero (POLY poly)
 {
-    std::cout << "No variables, only constants !" << std::endl;
-    std::cout << "Statement evaluates to: " << poly[0] << " = 0" << std::endl;
+    std::cout << "No variables, only constants !" << std::endl << std::endl;
+    std::cout << "Statement evaluates to : " << poly[0] << " = 0" << std::endl;
     if (poly[0] == 0)
         std::cout << "Statement is... TRUE !" << std::endl;
     else
@@ -57,8 +57,18 @@ void solve_zero (POLY poly)
 
 void solve_one (POLY poly)
 {
-    float sol = poly[0] * -1 / poly[1];
-    std::cout << "The solution is :" << std::endl;
+    float sol = -poly[0] / poly[1];
+    if (sol == -0.0f)
+        sol = 0;
+
+    std::cout << "Solving linearly :" << std::endl << std::endl;
+
+    std::cout << "    " << poly[0] << " " << (poly[1] > 0 ? "+" : "") << poly[1] << "x = 0" << std::endl;
+    std::cout << "<=> " << poly[1] << "x = " << -poly[0] << std::endl;
+    std::cout << "<=> " << poly[1] << "x / " << poly[1] << " = " << -poly[0] << " / " << poly[1] << std::endl;
+    std::cout << "<=> x = " << sol << std::endl << std::endl;
+
+    std::cout << "The unique solution is :" << std::endl;
     std::cout << sol << std::endl;
 }
 
@@ -68,23 +78,45 @@ void solve_two (POLY poly)
     float b = poly[1];
     float c = poly[0];
     float discr = b * b - 4 * a * c;
+
+    std::cout << "Computing discriminant :" << std::endl << std::endl;
+
+    std::cout << "    Δ = b² - 4ac" << std::endl;
+    std::cout << "<=> Δ = " << b << "² - 4 * " << a << " * " << c << std::endl;
+    std::cout << "<=> Δ = " << b * b << " - " << 4 * a * c << std::endl;
+    std::cout << "<=> Δ = " << discr << std::endl << std::endl;
+
+    float discr_abs = discr > 0 ? discr : -discr;
+    
+    std::cout << "Roots can be found by solving :" << std::endl << std::endl;
+
+    std::cout << "    x = ( -b ± √Δ ) / (2 * a)" << std::endl;
+    std::cout << "<=> x = ( -(" << b << ") ± √" << discr << " ) / (2 * " << a << ")" << std::endl;
+    std::cout << "<=> x = ( " << -b << " ± " << sqrt(discr_abs) << (discr < 0 ? "i" : "") << " ) / " << 2 * a << std::endl;
+    std::cout << "<=> x = " << -b << " / " << 2 * a << " ± " << sqrt(discr_abs) << (discr < 0 ? "i" : "") << " / " << 2 * a << std::endl;
+    std::cout << "<=> x = " << -b / (2 * a) << " ± " << sqrt(discr_abs) / (2 * a) << (discr < 0 ? "i" : "") << std::endl << std::endl;
+
+
     if (discr > 0) {
         float sol1 = (-b + sqrt(discr)) / (2 * a);
         float sol2 = (-b - sqrt(discr)) / (2 * a);
-        std::cout << "Discriminant is strictly positive, the two (real) solutions are:" << std::endl;
+
+        std::cout << "Discriminant is strictly positive, the two (real) solutions are :" << std::endl;
         std::cout << sol1 << std::endl;
         std::cout << sol2 << std::endl;
     }
     else if (discr < 0) {
         float real = -b / (2 * a);
         float complex = sqrt(-discr) / (2 * a);
-        std::cout << "Discriminant is strictly negative, the two (complex) solutions are:" << std::endl;
+    
+        std::cout << "Discriminant is strictly negative, the two (complex) solutions are :" << std::endl;
         std::cout << real << " + " << complex << "i" << std::endl;
         std::cout << real << " - " << complex << "i" << std::endl;
     }
     else {
-        float sol = -b;
-        std::cout << "Discriminant is strictly zero, the one solution is:" << std::endl;
+        float sol = -b / (2 * a);
+
+        std::cout << "Discriminant is strictly zero, the one solution is :" << std::endl;
         std::cout << sol << std::endl;
     }
 }
